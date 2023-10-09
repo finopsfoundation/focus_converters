68cc7cbcd2e7c08a94b5fd67b7e6bd69e12d971d
Varun Mittal <varunmittal91@gmail.com>
2023-10-08 21:41:23 -0400

    Added aws provider conversion config.
    Signed-off-by: Varun Mittal <varunmittal91@gmail.com>

---
+++ focus_converter_base/tests/provider_config_tests/aws/test_aws_provider.py
@@ -0,0 +1,32 @@
+from unittest import TestCase
+from unittest.mock import patch, PropertyMock
+
+import pandas as pd
+import polars as pl
+
+from focus_converter.configs.base_config import ConversionPlan
+from focus_converter.conversion_functions.column_functions import ColumnFunctions
+from focus_converter.converter import FocusConverter
+
+
+class TestAWSProvider(TestCase):
+    def test_aws_provider_config(self):
+        conversion_plan = ConversionPlan.load_yaml(
+            "focus_converter/conversion_configs/aws/D011_S001.yaml"
+        )
+
+        focus_converter = FocusConverter()
+        focus_converter.plans = {"aws": [conversion_plan]}
+        column_exprs = focus_converter.prepare_horizontal_conversion_plan(
+            provider="aws"
+        )
+
+        test_dataframe = pd.DataFrame([{"a": 1}])
+
+        test_pl_df = (
+            pl.from_dataframe(test_dataframe)
+            .lazy()
+            .with_columns(column_exprs)
+            .collect()
+        )
+        self.assertEqual(list(test_pl_df["Provider"])[0], "AWS")
