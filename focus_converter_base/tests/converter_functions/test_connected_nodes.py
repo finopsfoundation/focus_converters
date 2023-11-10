7aaee58df4899510739bd643b8b7a7eeacc66938
Varun Mittal <varunmittal91@gmail.com>
2023-11-10 15:43:22 -0500

    Updated connected_nodes test to allow ensuring provider conversion plans terminate to FOCUS columns.
    Signed-off-by: Varun Mittal <varunmittal91@gmail.com>

--- focus_converter_base/tests/converter_functions/test_connected_nodes.py
+++ focus_converter_base/tests/converter_functions/test_connected_nodes.py
@@ -1,7 +1,7 @@
+import re
 import tempfile
 from unittest import TestCase
 
-import polars
 import polars as pl
 
 from focus_converter.converter import FocusConverter
@@ -9,40 +9,25 @@ from focus_converter.data_loaders.data_loader import DataFormats, ParquetDataFor
 
 
 class TestConnectedNodes(TestCase):
-    def __genetic_fnc_connected_nodes__(self, provider):
+    @staticmethod
+    def __genetic_fnc_connected_nodes__(provider):
         # the only terminal node should be FOCUS column, ensuring that all columns are transformed
 
         converter = FocusConverter()
         converter.load_provider_conversion_configs()
         converter.prepare_horizontal_conversion_plan(provider=provider)
 
-        graph = converter.__network__.__prepare_graph__()
-        leaf_nodes = [
-            node for node, out_degree in graph.out_degree() if out_degree == 0
-        ]
-
-        for node in graph.nodes():
-            print(node)
-            print(list(graph.successors(node)))
-            print()
-
-        self.assertEqual(len(leaf_nodes), 1, "More than one terminal node found")
-        self.assertEqual(
-            set(leaf_nodes), {"Focus_Dataset"}, "Terminal node is not FOCUS column"
-        )
-
     def test_validate_aws_plan(self):
         self.__genetic_fnc_connected_nodes__(provider="aws")
 
-    #
-    # def test_validate_gcp_plan(self):
-    #     self.__genetic_fnc_connected_nodes__(provider="gcp")
-    #
-    # def test_validate_azure_plan(self):
-    #     self.__genetic_fnc_connected_nodes__(provider="azure")
-    #
-    # def test_validate_oci_plan(self):
-    #     self.__genetic_fnc_connected_nodes__(provider="oci")
+    def test_validate_gcp_plan(self):
+        self.__genetic_fnc_connected_nodes__(provider="gcp")
+
+    def test_validate_azure_plan(self):
+        self.__genetic_fnc_connected_nodes__(provider="azure")
+
+    def test_validate_oci_plan(self):
+        self.__genetic_fnc_connected_nodes__(provider="oci")
 
     def test_columns_check(self):
         lazy_frame = pl.DataFrame({"a": [1, 2, 3], "b": [1, 2, 3]}).lazy()
@@ -62,30 +47,9 @@ class TestConnectedNodes(TestCase):
                 export_path="/tmp/converted_aws",
                 export_include_source_columns=False,
             )
-            pre = converter.__process_lazy_frame__(lazy_frame)
-            print(pre)
-            raise ValueError
-            # print(pre.explain())
 
-            # converter.convert()
-            return
-            pre = converter.__process_lazy_frame__(lazy_frame)
-            try:
-                pre.collect()
-            except polars.exceptions.ColumnNotFoundError as e:
-                print(type(e.args[:100]), len(e.args))
-                print(len(e.args))
-                print(len(e.args[0]))
-
-                # print last 100 characters of the error message
-
-                last_lines = e.args[0].splitlines()
-
-                for line in last_lines[len(last_lines) - 100 : len(last_lines)]:
-                    print(line)
-                    # if "ErrorStateSync" in line:
-                    #     print(line)
-                pass
-            raise ValueError(
-                "Column not found in the dataframe. Please check the column names."
+            with self.assertRaises(ValueError) as cm:
+                converter.convert()
+            self.assertTrue(
+                re.match(r"Column\(s\) '.*' not found in data", str(cm.exception))
             )
